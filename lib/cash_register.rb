a43b6f9ea8f79ea8ae2e4d5a9da540e9b6cbea74
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-03-25 02:03:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+  
+  attr_accessor :cash_register, :cash_register_with_discount, :total
 
+
+  def initialize(total)
+    @total = 0 ||
+    self.cash_register_with_discount -= 20
+  end 
+
+end
+
+drawer = CashRegister.new(0)
+drawer.total
+drawer.cash_register_with_discount
